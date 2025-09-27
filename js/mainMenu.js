b97e54445a719fa9c304457744a9ab86ea3bb789
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2025-09-27 14:43:13 +0200

    implement main menu & run summary submenu

--- js/mainMenu.js
+++ js/mainMenu.js
@@ -1,5 +1,21 @@
 export default true;
-document.querySelector('header > #menu-btn').addEventListener('click', event => {
-    document.querySelector('header > menu').classList.remove('hidden');
+const mainMenu = document.querySelector('#main-menu');
+let activeElement;
+document.querySelector('header > #menu-btn').addEventListener('click', () => mainMenu.classList.toggle('hidden'));
+mainMenu.addEventListener('click', event => {
+    if (!(event.target instanceof HTMLButtonElement))
+        return;
+    activeElement = mainMenu.querySelector(`#${event.target.id.replace('-btn', '')}`);
+    activeElement.classList.add('child-no-hide');
+    mainMenu.classList.add('hide-children');
+});
+document.querySelector('#main-menu > #menu-back-btn').addEventListener('click', () => {
+    if (activeElement) {
+        activeElement.classList.remove('child-no-hide');
+        mainMenu.classList.remove('hide-children');
+        activeElement = undefined;
+    }
+    else
+        mainMenu.classList.toggle('hidden');
 });
 //# sourceMappingURL=mainMenu.js.map
--- ts/mainMenu.ts
+++ ts/mainMenu.ts
@@ -1,5 +1,24 @@
-export default true as boolean;
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 
-document.querySelector<HTMLButtonElement>('header > #menu-btn')!.addEventListener('click', event => {
-  document.querySelector('header > menu')!.classList.remove('hidden');
-})
+export default true as boolean;
+const mainMenu = document.querySelector<HTMLMenuElement>('#main-menu')!;
+let activeElement: HTMLDivElement | undefined;
+
+document.querySelector<HTMLButtonElement>('header > #menu-btn')!.addEventListener('click', () => mainMenu.classList.toggle('hidden'));
+
+mainMenu.addEventListener('click', event => {
+  if (!(event.target instanceof HTMLButtonElement)) return;
+
+  activeElement = mainMenu.querySelector<HTMLDivElement>(`#${event.target.id.replace('-btn', '')}`)!;
+  activeElement.classList.add('child-no-hide');
+  mainMenu.classList.add('hide-children');
+});
+
+document.querySelector<HTMLButtonElement>('#main-menu > #menu-back-btn')!.addEventListener('click', () => {
+  if (activeElement) {
+    activeElement.classList.remove('child-no-hide');
+    mainMenu.classList.remove('hide-children');
+    activeElement = undefined;
+  }
+  else mainMenu.classList.toggle('hidden');
+});
